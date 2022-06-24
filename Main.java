080e835e5c2059867a8baa3db7609b5923d58fe0
toldii1 <100698275+toldii@users.noreply.github.com>
2022-06-24 16:39:58 +0000

    2runda

--- Main.java
+++ Main.java
@@ -25,5 +25,27 @@ Scanner scanner = new Scanner(System.in);
         }
     } while (odp != los);
      System.out.println("BRAWO !!! odgadłeś za " + ii + "razem!");
-  }
-}
+    
+    int ii1 = 0;
+    int los1; 
+    int odp1;
+
+Random rnd1= new Random(); 
+    los1= rnd1.nextInt(100)+1;
+
+
+    System.out.println("Kolejna runda, tym razem coś trudniejszego");
+     System.out.println("Zgadnij liczbę od 1 do 100 ");
+    do{
+      ii1++;
+       System.out.println("Podaj liczbe: ");
+      odp1= scanner.nextInt();
+        if(odp1 > los1){
+           System.out.println("Niestety to nie ta liczba, spróbój podać mniejszą liczbę");
+        }
+         else if(odp1 < los1){
+           System.out.println("Niestety to nie ta liczba, spróbój podać większą liczbę");
+        }
+    } while (odp1 != los1);
+     System.out.println("BRAWO !!! odgadłeś za " + ii1 + "razem!");
+} }
